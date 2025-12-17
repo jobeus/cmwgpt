c3a9fd8a95c65e4a04c69f4dc5242ccb9e5299f6
Scotty Carmichael <jobeus@gmail.com>
2025-12-16 21:16:46 -0700

    fix to_file

--- src/services/openai_service.py
+++ src/services/openai_service.py
@@ -700,7 +700,7 @@ class OpenAIService:
                     )
                 else:  # assume gpt-image-1.5 or similar custom model
                     if edit_image:
-                        file_obj = edit_image.to_file()
+                        file_obj = await edit_image.to_file()
                         result = await client.images.edit(
                             model=model,
                             image=[file_obj],
